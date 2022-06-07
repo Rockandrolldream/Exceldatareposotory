714c9e111fe2825ec5d3b5e2fad355830104d4ed
Rockandrolldream <sa.jba@specialisterne.com>
2022-06-07 11:02:01 +0200

    setup gitrepo

---
+++ Mstestdatascipt/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Mstestdatascipt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodPoc()
+        {
+            Assert.AreEqual(2 ,2 );
+        }
+    }
+}
