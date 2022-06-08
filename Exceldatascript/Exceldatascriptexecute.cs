6a67ed4c01ce9b5d9ffd76874abc829a08bb12c8
Rockandrolldream <sa.jba@specialisterne.com>
2022-06-08 09:55:24 +0200

    excelupdate

--- Exceldatascript/Exceldatascriptexecute.cs
+++ Exceldatascript/Exceldatascriptexecute.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
+using System.Data;
+using Newtonsoft.Json;
 
 namespace Exceldatascript
 {
-    public class Exceldatascriptexecuteclass
+    public class Exceldatascriptexecute
     {
+
+        public  void GetDataTableFromExcel()
+        {
+            string content = "";
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            //FileInfo fileInfo = new FileInfo(path);
+            //ExcelPackage package = new ExcelPackage(fileInfo);
+            // ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault(); 
+
+            List<String> exceldata = new List<String>();
+
+            byte[] bin = File.ReadAllBytes("C:\\Users\\KOM\\Desktop\\Exceldatascriptopgave\\GRI_2017_2020.xlsx");
+
+            using (MemoryStream stream = new MemoryStream(bin)) {
+            using (ExcelPackage excelPackage = new ExcelPackage(stream))
+                {
+                    foreach (ExcelWorksheet worksheet in excelPackage.Workbook.Worksheets)
+                    {
+                        for (int i = worksheet.Dimension.Start.Row; i < worksheet.Dimension.End.Row; i++)
+                        {
+                            for (int j = worksheet.Dimension.Start.Column; j < worksheet.Dimension.End.Column; j++)
+                            {
+                                if (worksheet.Cells[i, j].Value != null)
+                                {
+                                    exceldata.Add(worksheet.Cells[i,j].Value.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+            
+            }
+
+            foreach (var item in exceldata)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
     }
 }
--- Exceldatascript/Program.cs
+++ Exceldatascript/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Exceldatascript
+﻿using System.Data;
+
+namespace Exceldatascript
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:/Users/KOM/Desktop/Exceldatascriptopgave/GRI_2017_2020.xlsx";
+            Exceldatascriptexecute exceldatascriptexecute = new Exceldatascriptexecute();
+
         }
     }
 }
